7e466f5ca5ce4ac286a43e3186294f3c508f5128
James Mallqui <2223110176@untels.edu.pe>
2025-04-08 15:32:05 -0500

    Modelo listo

---
+++ modelo/Libro.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Libro{
+	private String titulo;
+	public void setTitulo(String titulo){
+		
+	}
+	public String getTitulo(){
+		
+	}
+}
---
+++ modelo/Persona.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Persona{
+	private String nombre;
+	public void setNombre(String nombre){
+		
+	}
+	public String getNombre(){
+		
+	}
+}
